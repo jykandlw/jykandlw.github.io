2f5769e2972a8d349e5277baf32e5a55a4abeb7e
jykandlw <1119156420@qq.com>
2022-06-23 13:40:04 +0800

    Site updated: 2022-06-23 13:40:03

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "012",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "en",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "180",
+    "top": "8.5",
+    "key": "c8121aa8fcb84b75938b72885f2f8cb4"
+  }
+}
