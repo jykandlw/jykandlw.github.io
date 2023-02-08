6ced0c7ffe22eb40450355d4f0cc865b378f1bdf
jykandlw <1119156420@qq.com>
2023-02-08 22:40:35 +0800

    Site updated: 2023-02-08 22:40:28

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+// 动态心跳
+$(document).ready(function(e){
+    $('.copyright').html('©2022 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By JasperJian');
+})
---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "201",
+      "background": "5",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "en",
+      "borderRadius": "5",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "170",
+      "top": "9",
+      "key": "83297a0c8a7646f3a8fa1f904a02d2b3"
+    }
+  }
